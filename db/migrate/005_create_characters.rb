4e44310c60048e4cccda6389ed7705f64fbaeaa3
Oscar Ore <oscar.ore2011@gmail.com>
2020-07-26 19:56:12 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,5 +2,8 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |c|
       c.string :name 
+      c.integer :actor_id 
+      t.integer :show_id 
+    end 
   end
 end
